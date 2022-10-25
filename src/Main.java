fa1e8da317f1bc23f4b9dad826cef32de1b31f0e
tkachovvv <dmitiy.tk.skyeng@outlook.com>
2022-10-25 21:43:08 +0300

    task 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-    }
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Еще рано праздновать совершеннолетие.");
+        }
+        int schoolStudentAge = 25;
+        if (schoolStudentAge >= 7 && schoolStudentAge < 18) {
+            System.out.println("Ребенок ходит в школу.");
+        }
+        if (schoolStudentAge >= 18 && schoolStudentAge <24) {
+            System.out.println("Человек уже закончил школу и пора отправлять в университет.");
+        }
+        if (schoolStudentAge >= 24) {
+            System.out.println("Человек окончил университет и ему пора искать первую работу.");
+        }
+        }
 }
